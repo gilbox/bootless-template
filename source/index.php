c90456e35aa02be297c3d04a1c9a41c40d11c1b8
Gil Birman <g@gilbox.me>
2013-10-12 01:29:22 -0500

    First Commit

---
+++ source/index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title></title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+
+	<!-- Facebook -->
+	<meta property="og:title" content="" />
+	<meta property="og:description" content="" />
+	<meta property="og:url" content="" />
+	<meta property="og:image" content="" />
+
+	<!-- Windows 8 -->
+	<meta name="application-name" content="" />
+	<meta name="msapplication-TileColor" content="" />
+	<meta name="msapplication-TileImage" content="" />
+
+	<!-- Twitter -->
+	<meta name="twitter:card" content="">
+	<meta name="twitter:site" content="">
+	<meta name="twitter:title" content="">
+	<meta name="twitter:description" content="">
+	<meta name="twitter:url" content="">
+
+	<!-- 16x16 or 32x32 -->
+	<!--<link rel="shortcut icon" href="/img/favicon.ico" />-->
+
+	<!-- 114x114 -->
+	<link rel="apple-touch-icon" href="_/img/apple-touch-icon.png" />
+
+	<!-- TODO: Compile CSS before deployment -->
+
+	<!-- FontAwesome is a good option if you need icons in your bootstrap components,
+			but for more customization use http://icomoon.io/app/ -->
+
+	<!--<link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">-->
+	<!--[if IE 7]>
+	<!--<link href="assets/font-awesome/css/font-awesome-ie7.min.css" rel="stylesheet">-->
+	<![endif]-->
+
+	<link href="assets/css/styles.css" rel="stylesheet">
+
+	<!--[if lt IE 8]>
+	<script src="assets/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script> <![endif]-->
+
+</head>
+
+<body>
+
+<!-- Uncomment for the standard bootstrap navbar: -->
+<!--<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">-->
+	<!--<div class="container">-->
+		<!--<div class="navbar-header">-->
+			<!--<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">-->
+				<!--<span class="sr-only">Toggle navigation</span>-->
+				<!--<span class="icon-bar"></span>-->
+				<!--<span class="icon-bar"></span>-->
+				<!--<span class="icon-bar"></span>-->
+			<!--</button>-->
+			<!--<a class="navbar-brand" href="index.php">Start Bootstrap</a>-->
+		<!--</div>-->
+
+		<!--&lt;!&ndash; Collect the nav links, forms, and other content for toggling &ndash;&gt;-->
+		<!--<div class="collapse navbar-collapse navbar-ex1-collapse">-->
+			<!--<ul class="nav navbar-nav">-->
+				<!--<li><a href="#about">About</a></li>-->
+				<!--<li><a href="#services">Services</a></li>-->
+				<!--<li><a href="#contact">Contact</a></li>-->
+			<!--</ul>-->
+		<!--</div>&lt;!&ndash; /.navbar-collapse &ndash;&gt;-->
+	<!--</div>&lt;!&ndash; /.container &ndash;&gt;-->
+<!--</nav>-->
+
+<div class="container">
+
+	<header>
+			<h1>My bootstrap-y template...</h1>
+			<p>Pretty much a blank slate...</p>
+	</header>
+
+	<article>
+
+	</article>
+
+	<footer>
+
+	</footer>
+
+</div><!-- /.container -->
+
+<!-- TODO: Compile all JS before deployment -->
+
+<!-- TODO: switch to CDN before deployment: -->
+<!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>-->
+<script src="assets/js/vendor/jquery-1.10.1.js"></script>
+<script>window.jQuery || document.write("<script src='assets/js/vendor/jquery-1.10.1.min.js'>\x3C/script>")</script>
+
+<script src="assets/js/vendor/bootstrap.js"></script>
+
+<script src="assets/js/script.js"></script>
+
+<!-- Asynchronous google analytics; this is the official snippet.
+	 Replace UA-XXXXXX-XX with your site's ID and uncomment to enable.
+
+<script>
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-XXXXXX-XX']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+-->
+</body>
+</html>
